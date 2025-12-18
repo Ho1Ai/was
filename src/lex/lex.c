c0158868591c7fca8a04cc3616ad3a71bb2c2a23
Ho1Ai <koekto33309@gmail.com>
2025-12-18 23:59:41 +0300

    started writing lexer

---
+++ src/lex/lex.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+typedef enum {
+	TOKEN_IDENTIFIER,
+	TOKEN_NUMBER,
+	TOKEN_REGISTER,
+	TOKEN_STRING,
+
+	TOKEN_COMMA,
+	TOKEN_COLON,
+	TOKEN_LBRACKET,
+	TOKEN_RBRACKET,
+	TOKEN_PLUS,
+	TOKEN_MINUS,
+	TOKEN_ASTERISK,
+	
+	TOKEN_DIRECTIVE,
+	
+	TOKEN_NEWLINE,
+	TOKEN_EOF,
+	} TokenTypeEnum;
+
+typedef struct {
+	TokenTypeEnum type;
+	char* lexeme;
+	int line;
+	int column; // for debug
+	} LexerToken
---
+++ src/lex/lex.h
@@ -0,0 +1,31 @@
+#ifndef LEXER
+#define LEXER
+
+typedef enum {
+	TOKEN_IDENTIFIER,
+	TOKEN_NUMBER,
+	TOKEN_REGISTER,
+	TOKEN_STRING,
+
+	TOKEN_COMMA,
+	TOKEN_COLON,
+	TOKEN_LBRACKET,
+	TOKEN_RBRACKET,
+	TOKEN_PLUS,
+	TOKEN_MINUS,
+	TOKEN_ASTERISK,
+	
+	TOKEN_DIRECTIVE,
+	
+	TOKEN_NEWLINE,
+	TOKEN_EOF,
+	} TokenTypeEnum;
+
+typedef struct {
+	TokenTypeEnum type;
+	char* lexeme;
+	int line;
+	int column;
+	} LexerToken;
+
+#endif
